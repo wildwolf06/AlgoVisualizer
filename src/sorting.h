a2eda72d0c13721d3c54ea133ee7e7d43d82b36c
Pegasus717 <raghu59770@gmail.com>
2026-03-30 15:59:26 +0530

    add random array generator

--- src/sorting.h
+++ src/sorting.h
@@ -56,7 +56,15 @@ inline int countSwaps(const vector<Step>& steps, int upTo) {
     return count;
 }
 
-
+inline vector<int> makeRandomArray(int n) {
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) arr[i] = i + 1;
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        swap(arr[i], arr[j]);
+    }
+    return arr;
+}
 
 
 #endif 
